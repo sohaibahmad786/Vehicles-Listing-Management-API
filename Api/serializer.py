ca94925d24981c93940afd49998aa77f6979da4c
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-05-17 10:25:50 +0500

    Update serializer.py

--- Api/serializer.py
+++ Api/serializer.py
@@ -1,12 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Register
-from .models import Search_data
-from .models import Students
-from .models import Task
-from .models import Booking
-from .models import Message
-from .models import Person
 from .models import Company
 from .models import Cars
         
@@ -19,44 +13,6 @@ class Register_serializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data['password']=make_password(validated_data['password'])
         return super().create(validated_data)
-
-class Search_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Search_data
-        fields='__all__'
-
-class Student_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Students
-        fields='__all__'
-
-class Task_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Task
-        fields='__all__'
-
-class Booking_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Booking
-        fields='__all__'
-        extra_kwargs={
-           'user':{'read_only':True}
-        }
-
-class Message_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Message
-        fields='__all__'
-        extra_kwargs={
-            'sender':{'read_only':True}
-        }
-
-class Person_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Person
-        fields='__all__'
-
-
 
 class Cars_serializer(serializers.ModelSerializer):
     class Meta:
