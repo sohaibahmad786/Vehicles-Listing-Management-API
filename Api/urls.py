daae47f0ca9c02e1d0b32952cfeda32fe75dcffb
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-05-17 10:28:21 +0500

    Update urls.py

--- Api/urls.py
+++ Api/urls.py
@@ -1,18 +1,11 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from .views import Register_view,Register_detail
-from .views import Search_view
-from .views import Studentlist,Studentdetail
-from .views import Task_listview,Task_detailview
-from .views import Bookinglistview,AvailableSlotView
-from .views import MessagelistView,Chatlistview
-from .views import PersonViewSet
 from .views import CompanyView
 from .views import CarView
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
-router.register('person',PersonViewSet)
 router.register('company',CompanyView)
 router.register('car',CarView)
 
@@ -21,14 +14,5 @@ urlpatterns = [
    path('register/<int:pk>/',Register_detail.as_view()),
    path('login/',TokenObtainPairView.as_view()),
    path('login/refresh/',TokenRefreshView.as_view()),
-   path('search/',Search_view.as_view()),
-   path('student/',Studentlist.as_view()),
-   path('student/<int:pk>/',Studentdetail.as_view()),
-   path('task/',Task_listview.as_view()),
-   path('task/<int:pk>/',Task_detailview.as_view()),
-   path('book/',Bookinglistview.as_view()),
-   path('available/',AvailableSlotView.as_view()),
-   path('message/',MessagelistView.as_view()),
-   path('chat/',Chatlistview.as_view()),
    path('',include(router.urls)),
 ]
